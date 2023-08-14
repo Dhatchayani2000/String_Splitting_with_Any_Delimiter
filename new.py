a3420bbe8329ba720e16f2ce6c092d499cf27073
Dhatchayani2000 <141829867+Dhatchayani2000@users.noreply.github.com>
2023-08-14 15:54:51 +0530

    FIRST COMMIT

--- new.py
+++ new.py
@@ -1,3 +1,23 @@
 import re
-output = re.split(r'\W+','sdfkdjsadfsd diweiw; 1231:foo')
+
+class StringSplitter:
+    def __init__(self):
+        self.delimiter = None
+
+    def set_delimiter(self, delimiter):
+        self.delimiter = delimiter
+
+    def split_string(self, input_string):
+        if self.delimiter is None:
+            raise ValueError("Delimiter not set. Please use set_delimiter() method first.")
+
+        delimiter_pattern = re.escape(self.delimiter)
+        fields = re.split(delimiter_pattern, input_string)
+        return fields
+
+given_input = 'stigwith Delimiter; 1234:split12'
+splitter = StringSplitter()
+delimiter = input("Enter the delimiter: ")
+splitter.set_delimiter(delimiter)
+output = splitter.split_string(given_input)
 print(output)
